6c652c9d1e6443be80b55bc28bfb09aeec2e5390
Eduardo Menezes <edu_menez86@hotmail.com>
2014-04-10 12:15:13 -0300

    Prueba commit

---
+++ prueba.c
@@ -0,0 +1,13 @@
+/*
+ * prueba.c
+ *
+ *  Created on: 10/04/2014
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+int main (void){
+	printf ("Hola Mundo!! \n");
+	printf ("Con esto si que apruebo\n");
+			return 0;
+}
